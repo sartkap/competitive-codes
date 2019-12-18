bb7219ee285e00efe522d2f28fda60f193b04458
Sarthak Kapoor <sarthakkapoor76@gmail.com>
2019-12-19 00:23:08 +0530

    Create modulo_easy.cpp

---
+++ num_theory/modulo_easy.cpp
@@ -0,0 +1,42 @@
+struct mint{
+	uint v;
+	mint(ll vv=0){s(vv%mod+mod);}
+	mint& s(uint vv){
+		v=vv<mod?vv:vv-mod;
+		return *this;
+	}
+	mint operator-()const{return mint()-*this;}
+	mint& operator+=(const mint&rhs){return s(v+rhs.v);}
+	mint&operator-=(const mint&rhs){return s(v+mod-rhs.v);}
+	mint&operator*=(const mint&rhs){
+		v=ull(v)*rhs.v%mod;
+		return *this;
+	}
+	mint&operator/=(const mint&rhs){return *this*=rhs.inv();}
+	mint operator+(const mint&rhs)const{return mint(*this)+=rhs;}
+	mint operator-(const mint&rhs)const{return mint(*this)-=rhs;}
+	mint operator*(const mint&rhs)const{return mint(*this)*=rhs;}
+	mint operator/(const mint&rhs)const{return mint(*this)/=rhs;}
+	mint pow(int n)const{
+		mint res(1),x(*this);
+		while(n){
+			if(n&1)res*=x;
+			x*=x;
+			n>>=1;
+		}
+		return res;
+	}
+	mint inv()const{return pow(mod-2);}
+	/*mint inv()const{
+		int x,y;
+		int g=extgcd(v,mod,x,y);
+		assert(g==1);
+		if(x<0)x+=mod;
+		return mint(x);
+	}*/
+	friend ostream& operator<<(ostream&os,const mint&m){
+		return os<<m.v;
+	}
+	bool operator<(const mint&r)const{return v<r.v;}
+	bool operator==(const mint&r)const{return v==r.v;}
+};
