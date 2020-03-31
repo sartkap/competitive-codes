a0f43b94a4e1c53e6247bfa4d13c32bbcf4a7d51
Sarthak Kapoor <sarthakkapoor76@gmail.com>
2020-03-31 22:52:48 +0530

    Update manacher.cpp

--- strings/manacher.cpp
+++ strings/manacher.cpp
@@ -81,7 +81,7 @@ int main() {
 
 	
 	// O(n) algorithm to calculate d1 and d2
-
+	// (l,r) is the palindrome found so far with the largest value of r
 	// for d1[i];
 	for(int i=0,l=0,r = -1;i<n;i++){
 		int k = (i > r) ? 1 : min(d1[l+r-i],r-i+1);
