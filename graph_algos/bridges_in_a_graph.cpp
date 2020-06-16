7df4bfd84ae6723f855e24074b80599c60512005
Sarthak Kapoor <sarthakkapoor76@gmail.com>
2020-06-16 13:52:16 +0530

    Create bridges_in_a_graph.cpp

---
+++ graph_algos/bridges_in_a_graph.cpp
@@ -0,0 +1,117 @@
+// Author : Sarthak Kapoor
+#include<bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+#define rep(i,n) for(int i=0;i<n;++i)
+#define repa(i,a,n) for(int i=a;i<n;++i)
+#define repr(i,n) for(int i=n-1;i>=0;--i)
+#define repba(i,b,a) for(int i=b;i>=a;--i)
+#define repab(i,a,b) for(int i=a;i<b;++i)
+#define ll long long
+#define pi pair<int,int> 
+#define pii pair<pi,int>
+#define ull unsigned long long
+#define vi vector<int>
+#define viip vector<pair<int,pair<int,int> > > 
+#define mp make_pair
+#define vip vector<pair<int,int> >
+#define pb push_back 
+#define fi first
+#define sec second
+#define all(v) v.begin(),v.end()
+#define s(v) v.size()
+ll mod = 1000000007;
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());//use rng()%n for numbers in range [0,n-1]
+ll gcd(ll a,ll b){
+	if(b==0)return a;
+	return gcd(b,a%b);
+}
+ll fastexp(ll x,ll a){
+    ll res = 1;
+    while(a>0){
+        if(a&1){
+            res = (res*x)%mod;
+        }
+        a=a>>1;
+        x=(x*x)%mod;
+    }
+    return res;
+}
+ll inverse(ll n){
+    return fastexp(n,mod-2);
+}
+template <typename T>
+void add(T &a, T b){
+	a += b;
+	if(a >= mod)a -= mod;
+}
+template <typename T>
+void sub(T &a, T b){
+	a -= b;
+	if(a < 0)a += mod;
+}
+template <typename T>
+void mul(T &a, T b){
+	a *= b;
+	if(a >= mod)a %= mod;
+}
+const int maxn = 1e5 + 2;
+vi v[maxn];
+set<int> tr[maxn];
+bool vis[maxn];
+int dp[maxn];
+int in[maxn];
+int par[maxn];
+map<pi,int> ans;
+void dfs_tree(int u,int p){
+	vis[u] = 1;
+	in[u] = 1;
+	for(auto x:v[u]){
+		if(!vis[x]){
+			tr[x].insert(u);
+			tr[u].insert(x);
+			dfs_tree(x,u);
+		}
+	}
+	in[u] = 2;
+}
+void dfs(int u,int p){
+	vis[u] = 1;
+	par[u] = p;
+	for(auto x:v[u]){
+		auto it = tr[u].find(x);
+		if(it == tr[u].end()){
+			if(vis[x])dp[u]++;
+			else dp[u]--;
+		}
+	}
+	for(auto x:tr[u]){
+		if(x == p)continue;
+		dfs(x,u);
+		dp[u] += dp[x];
+	}
+}
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+	int n,m;
+	cin>>n>>m;
+	rep(i,m){
+		int x,y;
+		cin>>x>>y;
+		v[x].pb(y);
+		v[y].pb(x);
+	}
+	dfs_tree(1,1);
+	rep(i,n+1)vis[i] = 0;
+	dfs(1,1);
+	int f = 0;
+	repa(i,2,n+1){
+		if(dp[i] == 0){
+			// bridge connecting i with its parent 
+		}
+	}
+	return 0;
+}
