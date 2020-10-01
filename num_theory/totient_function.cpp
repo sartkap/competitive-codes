9b2b2c406c0fe2f103601274746710e9008415f4
Varun Goyal <42479771+govarun@users.noreply.github.com>
2020-10-01 16:46:23 +0530

    Create totient_function.cpp

---
+++ num_theory/totient_function.cpp
@@ -0,0 +1,12 @@
+// https://cp-algorithms.com/algebra/phi-function.html
+void using_divisor_sum(int n) {
+    vector<int> phi(n + 1);
+    phi[0] = 0;
+    phi[1] = 1;
+    for (int i = 2; i <= n; i++)
+        phi[i] = i - 1;
+
+    for (int i = 2; i <= n; i++)
+        for (int j = 2 * i; j <= n; j += i)
+              phi[j] -= phi[i];
+}
